bc5e687199329ae0920e4a13aa17deee3efd11a9
Meow520 <cgjg0289@mail4.doshisha.ac.jp>
2025-01-02 23:26:54 +0900

    [add] exercise 2 example

--- 2/exercise2.py
+++ 2/exercise2.py
@@ -7,3 +7,70 @@
 3. 実行ファイルとクラスを定義するファイルは分ける
 """
 
+from exercise2_class import Enemy, MainCharacter
+import time
+
+print("戦闘ゲームにようこそ。")
+diff_input = input("「簡単」・「普通」・「難しい」から難易度を選択してください: ")
+if diff_input == "簡単":
+    diff = "easy"
+elif diff_input == "普通":
+    diff = "normal"
+elif diff_input == "難しい":
+    diff = "hard"
+    
+name = input("名前を入力してください: ")
+main = MainCharacter(name=name)
+print(f"{main.name}はHP{main.hp}と基本攻撃{main.skills.base.damage}を得た！")
+
+enemy_name = ["ダスクファング", "ナイトテラー", "ヴォイドエンフォーサー"]
+
+count = 0
+for i in range(len(enemy_name)):
+    enemy = Enemy(name=enemy_name[i], difficulty=diff, index=i)
+    print(f"敵の{enemy.name}が現れた！")
+    time.sleep(2)
+    while True:
+        print(f"{main.name}のHP: {main.hp} / 敵{enemy.name}のHP: {enemy.hp}")
+
+        if count == 6:
+            print(f"{main.name}の特殊攻撃{main.skills.special.damage}を得た。", end=" ")
+            if main.skills.special.self_damage != 0:
+                print(f"なお、特殊攻撃1回につき自分のHPが{main.skills.special.self_damage}減る。")
+            if main.skills.special.self_healing != 0:
+                print(f"なお、特殊攻撃1回につき自分のHPが{main.skills.special.self_healing}回復する。")
+            time.sleep(2)
+        
+        # 自分のターン
+        if count >= 6:
+            attack_type = input("「基本攻撃」と「特殊攻撃」から攻撃を選んで入力してください: ")
+            if attack_type == "基本攻撃":
+                print(f"{main.name}は{enemy.name}に基本攻撃{main.skills.base.damage}をした！")
+                damage = main.attack(type="base")
+            else:
+                print(f"{main.name}は{enemy.name}に特殊攻撃{main.skills.special.damage}をした！")
+                damage = main.attack(type="special")
+        else:
+            attack_type = input("「基本攻撃」から攻撃を選んで入力してください: ")
+            print(f"{main.name}は{enemy.name}に基本攻撃{main.skills.base.damage}をした！")
+            damage = main.attack(type="base")
+        enemy.get_damage(damage=damage)
+        count += 1
+        time.sleep(2)
+        
+        if main.is_dead:
+            print("YOU LOSED....")
+            exit()
+        
+        # 敵のターン
+        damage = enemy.attack()
+        main.get_damage(damage=damage)
+        print(f"{enemy.name}は{main.name}に攻撃{enemy.skill.damage}をした！")
+        time.sleep(2)
+        if enemy.is_dead:
+            print(f"敵の{enemy.name}を倒した！")
+            time.sleep(2)
+            break
+        
+    
+print("YOU WON!")
--- 2/exercise2_class.py
+++ 2/exercise2_class.py
@@ -1,3 +1,97 @@
 """
 練習問題クラス定義用ファイル
 """
+
+import random as rnd
+from typing import Dict, Optional, Tuple, Union, Literal
+
+
+class Skill:
+    def __init__(
+        self,
+        damage: int,
+        self_damage: Optional[int] = None,
+        self_healing: Optional[int] = None,
+    ):
+        self.damage = damage
+        self.self_damage = self_damage
+        self.self_healing = self_healing
+
+class Character:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.is_dead = False
+
+    def get_damage(self, damage: int) -> None:
+        self.hp -= damage
+        if self.hp <= 0:
+            self.hp = 0
+            self.is_dead = True
+
+class MainCharacterSkills:
+    def __init__(self, base_damage: int,special_damage: int,self_damage: int,self_healing: int):
+        self.base = Skill(damage=base_damage)
+        self.special = Skill(damage=special_damage, self_damage=self_damage, self_healing=self_healing)
+
+class MainCharacter(Character):
+    def __init__(self, name: str):
+        super().__init__(name=name)
+        self.hp, self.skills = self.__gain_abilities()
+
+    def __gain_abilities(
+        self,
+    ) -> Tuple[int, Dict[str, MainCharacterSkills]]:
+        hps = [150, 160, 170, 180, 190, 200]
+        hp = rnd.choice(hps)
+        base_damage = 25 if hp < 180 else 20
+        special_damage = rnd.choice([50, 60, 70, 80])
+
+        if hp >= 190:
+            if special_damage >= 70:
+                self_damage = 10
+            else:
+                self_damage = 5
+            self_healing = 0
+        else:
+            self_damage = 0
+            self_healing = 10
+
+        skill = MainCharacterSkills(
+            base_damage=base_damage,
+            special_damage=special_damage,
+            self_damage=self_damage,
+            self_healing=self_healing,
+        )
+        return hp, skill
+
+    def attack(self, type: Literal["base", "special"]) -> int:
+        if type == "base":
+            damage = self.skills.base.damage
+
+        else:
+            damage = self.skills.special.damage
+            self.hp -= self.skills.special.self_damage
+            self.hp += self.skills.special.self_healing
+        return damage
+    
+        
+class Enemy(Character):
+    def __init__(self, name: str, difficulty: Literal["easy", "normal", "hard"], index: int):
+        super().__init__(name=name)
+        self.difficulty = difficulty
+        self.index = index + 1
+        self.hp, self.skill = self.__gain_abilities()
+        
+    def __gain_abilities(self) -> Tuple[int, Skill]:
+        if self.difficulty == "easy":
+            hp = 20 * self.index
+        elif self.difficulty == "normal":
+            hp = 40 * self.index
+        else:
+            hp = 60 * self.index
+
+        skill = Skill(damage=hp // 6)
+        return hp, skill
+    
+    def attack(self) -> int:
+        return self.skill.damage
